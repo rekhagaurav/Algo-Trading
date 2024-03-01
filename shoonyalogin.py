5b9e42d2da180845264a6fc801824b0eeecdb3fd
rekhagaurav <155154200+rekhagaurav@users.noreply.github.com>
2024-03-01 12:19:47 +0530

    Add files via upload

--- shoonyalogin.py
+++ shoonyalogin.py
@@ -1,7 +1,10 @@
-import sys
-from datetime import datetime, time, timedelta
+import sys,time
+from datetime import datetime,  timedelta
+
+import data
+import results
 from NorenRestApiPy.NorenApi import NorenApi
-from algo import app
+
 
 # login page
 class ShoonyaApiPy(NorenApi):
@@ -10,6 +13,12 @@ class ShoonyaApiPy(NorenApi):
         self.get = None
         global api
         api = self
+        print(self)
+
+    def get_ltp_data(self, param, param1, param2):
+        pass
+
+
 import logging
 import pyotp
 logging.basicConfig(level=logging.DEBUG)
@@ -35,50 +44,41 @@ vc = 'FA71897_U'
 app_key = '036e0ee3e72de505cd7fe9cbcf16c7e8'
 imei = 'abc1234'
 ret1 = api.login(userid=user, password=pwd, twoFA=factor2, vendor_code=vc, api_secret=app_key, imei=imei)
-print(ret1)
-
-
-exch  = 'NSE'
-token = '3045'
-ret2 = api.get_security_info(exchange='NSE', token='3045')
-api.searchscrip(exchange='NSE', searchtext='REL')
-print(ret2)
+print("login result=",ret1)
 
 exch  = 'NSE'
 token = '3045'
-ret3 = api.get_quotes(exchange='NSE', token='3045')
-print(ret3)
+# quote = api.get_quotes(exchange='NSE', token='3045')
+# print("quote=",quote)
 
-lastBusDay = datetime.datetime.today()
-lastBusDay = lastBusDay.replace(hour=0, minute=0, second=0, microsecond=0)
-ret4 = api.get_time_price_series(exchange='NSE', token='3045', starttime=lastBusDay.timestamp(), interval=5)
-print(ret4)
+# lastBusDay = datetime.datetime.today()
+# lastBusDay = lastBusDay.replace(hour=0, minute=0, second=0, microsecond=0)
+# ret4 = api.get_time_price_series(exchange='NSE', token='3045', starttime=lastBusDay.timestamp(), interval=5)
+# print(ret4)
+#
+# ret5 =api.get_daily_price_series(exchange="NSE",tradingsymbol="SBIN-EQ",startdate="457401600",enddate="480556800")
+# print(ret5)
 
-ret5 =api.get_daily_price_series(exchange="NSE",tradingsymbol="SBIN-EQ",startdate="457401600",enddate="480556800")
-print(ret5)
 
-ret6 = api.place_order(buy_or_sell='B', product_type='C',
-                        exchange='NSE', tradingsymbol='SBIN-EQ',
-                        quantity=1, discloseqty=0,price_type='SL-LMT', price=200.00, trigger_price=199.50,
-                        retention='DAY', remarks='my_order_001')
-print(ret6)
 
-time.sleep(0.4)
+#time.sleep(0.4)
 
 # Initialize sell order parameters
+
 buyOrderParams = {
-    "variety": "NORMAL",
-    "tradingsymbol": "SBIN-EQ",
-    "symboltoken": "3045",
-    "transactiontype": "BUY",
-    "exchange": "NSE",
-    "ordertype": "MARKET",
+    "buy_or_sell" : "B",
     "producttype": "INTRADAY",
+    "exchange": "NSE",
+    "tradingsymbol": "SBIN-EQ",
+    'quantity': 'minBuyQty',
+    'discloseqty': '0',
+    "ordertype": "MARKET",
+    'price':'200.00',
+    'trigger_price': '199.50',
     "duration": "DAY",
-    "price": "0",
-    "squareoff": "0",
-    "stoploss": "0",
-    "quantity": minBuyQty
+    'remarks': 'my_order_001',
+    "variety": "NORMAL",
+    "symboltoken": "3045",
 }
 sellOrderParams = {
     "variety": "NORMAL",
@@ -87,50 +87,77 @@ sellOrderParams = {
     "transactiontype": "SELL",
     "exchange": "NSE",
     "ordertype": "MARKET",
+    'discloseqty': '0',
     "producttype": "INTRADAY",
     "duration": "DAY",
     "price": "0",
     "squareoff": "0",
     "stoploss": "0",
-    "quantity": minBuyQty
+    "quantity": "minBuyQty"
 }
 
 
 # Initial cash check
-initialCash = NorenApi.rmsLimit().get("data").get("availablecash")
+initialCash = api.get_limits()
 time.sleep(0.4)
 if initialCash is None:
     initialCash = 0
-print("Initial available Cash =", initialCash)
+print("Initial available Cash =", initialCash['cash'])
 
 start_time = datetime.now()
 EndTime = start_time + timedelta(minutes=duration)
 print("EndTime:", EndTime)
 
+# orderStatus= api.place_order(buy_or_sell='B', product_type='i',
+#                         exchange='NSE', tradingsymbol='SBIN-EQ',
+#                         quantity=1, discloseqty=0,price_type='SL-LMT', price=200.00, trigger_price=199.50,
+#                         retention='DAY', remarks='my_order_001')
+# print("orderStatus=", orderStatus)
+orderStatus= api.place_order(buy_or_sell='B', product_type='C',
+                         exchange='NSE', tradingsymbol='SBIN-EQ',
+                         quantity=1, discloseqty=0,price_type='MKT', price=500.00,
+                         retention='DAY', remarks='my_order_001')
+#print("orderStatus=", orderStatus)    # buy first order
+print("orderStatus=", orderStatus)
+time.sleep(0.4)
+
+
 #===================   start Algo trade =====================
-NorenApi.placeOrder(buyOrderParams)    # buy first order
+
+order_book = api.get_order_book()
+print("order_book=", order_book)
+results=[]
 time.sleep(0.4)
-orderbook = NorenApi.orderBook()
-time.sleep(0.4)
-for item in orderbook.get('data'):
-    obStatus = item.get('orderstatus')
-    obText = item.get('text')
-    print("Orderbook Status=", obStatus, ", text=", obText)
+for item in order_book:
+    Status = item.get('status')
+    exch = item.get('exch')
+    tsym = item.get('tsym')
+    qty = item.get('qty')
+    rejreason = item.get('rejreason')
+
+    result = (Status, exch, tsym, qty, rejreason)
+    results.append(result)
+print(results)
 time.sleep(0.4)
 
 
 #----------------- check current position -------------
-position = NorenApi.position()
+position = api.get_positions()
 time.sleep(0.4)
-data = position.get('data')
-qty = 0;
-for item in data:
-        qty = item.get("netqty")
-        buyprice = item.get("avgnetprice")
-        print("Quantity:", qty)
-        print("Average Net Price:", buyprice)
+data = api.get_positions()
+if position is not None:
+    qty = 0
+    results=[]
+    for item in position:
 
-# print("Initial Holding position QTY =", position)
+        qty = item.get("Quantity")
+        buyprice = item.get("avgnetprice")
+        result = ("Quantity:", qty, "Average Net Price:", buyprice)
+        results.append(result)
+print(results)
+
+
+#print("Initial Holding position QTY =", position)
 if qty is None:
     qty = 0
 print("Initial Holding position QTY =", qty)
@@ -138,47 +165,47 @@ print("Initial Holding position QTY =", qty)
 sellOrderParams["quantity"] = minBuyQty
 # Fetch LTP data
 time.sleep(1)
-x = NorenApi.ltpData("NSE", "SBIN-EQ", "3045")
+x = api.get_ltp_data("NSE", "SBIN-EQ", "3045")
 time.sleep(0.4)
-ltp = float(x.get('data').get('ltp'))
-print("LTP =", ltp)
-
-# Get position
-time.sleep(0.4)
-position = NorenApi.position()
-time.sleep(0.4)
-data = position.get('data')
-for item in data:
-    qty = item.get("netqty")
-    buyprice = item.get("avgnetprice")
-    print("Quantity:", qty)
-    print("Average Net Price:", buyprice)
-
+if x is not None:
+    ltp = float(x.get('data',{}).get('ltp',0))
+    print("LTP =", ltp)
+if position is not None:
+    data = position.get('data')
+    if data is not None:
+        qty = 0
+        results = []
+        for item in data:
+            qty = item.get("Quantity")
+            buyprice = item.get("avgnetprice")
+            result = ("Quantity:", qty, "Average Net Price:", buyprice)
+            results.append(result)
+        print(results)
     if qty is None:
         qty = 0
     if buyprice is None:
         buyprice = 0
 
     # calculate avgPrice
-if ltp < float(buyprice) - float(StopLossInRs):
-    sellOrderParams["quantity"] = qty
-    time.sleep(0.4)
-    NorenApi.placeOrder(sellOrderParams)
-    time.sleep(0.4)
+    if ltp < float(buyprice) - float(StopLossInRs):
+        sellOrderParams["quantity"] = qty
+        time.sleep(0.4)
+        api.place_Order(sellOrderParams)
+        time.sleep(0.4)
     print("Stop loss hit : ", qty, "qty sold at market price")
     sys.exit()
 
-if ltp > (float(buyprice) + float(ExitDiffPrice)) and (float(qty) > 0):  # book profit
-    time.sleep(0.4)
-    NorenApi.placeOrder(sellOrderParams)
-    time.sleep(0.4)
+    if ltp > (float(buyprice) + float(ExitDiffPrice)) and (float(qty) > 0):  # book profit
+        time.sleep(0.4)
+        api.place_Order(sellOrderParams)
+        time.sleep(0.4)
     print("Profit booked : ", qty, "qty sold at =", ltp, " market price, buyprice=", buyprice, "ExitDiffPrice=",
           ExitDiffPrice)
 
-if (ltp < (float(buyprice) - float(EntryDiffPrice))) and (float(qty) <= float(MaxOpenPosition)):
-    time.sleep(0.4)
-    NorenApi.placeOrder(buyOrderParams)
-    time.sleep(0.4)
+    if (ltp < (float(buyprice) - float(EntryDiffPrice))) and (float(qty) <= float(MaxOpenPosition)):
+        time.sleep(0.4)
+        api.placeOrder(buyOrderParams)
+        time.sleep(0.4)
     print("Purchased ", qty, "at ", ltp)
 
 # check end time
@@ -188,7 +215,7 @@ if current_time > EndTime:
     print("timespent:", timespent, "start time:", start_time, "current_time:", current_time)
     # Place sell order for all QTY
     time.sleep(0.4)
-    position = NorenApi.position()
+    position = api.position()
     time.sleep(0.4)
     data = position.get('data')
     for item in data:
@@ -202,7 +229,7 @@ if current_time > EndTime:
 
     if float(qty) > 0:
         sellOrderParams["quantity"] = qty
-        sellOrderResponse = NorenApi.placeOrder(sellOrderParams)
+        sellOrderResponse = api.placeOrder(sellOrderParams)
         time.sleep(0.4)
         if sellOrderResponse is not None and 'data' in sellOrderResponse and 'orderid' in sellOrderResponse['data']:
             order_id = sellOrderResponse['data']['orderid']
@@ -216,25 +243,33 @@ if current_time > EndTime:
 
 # End of loop
 time.sleep(1)
-balanceCash = NorenApi.rmsLimit().get("data").get("availablecash")
-time.sleep(0.4)
-if balanceCash is None:
-    balanceCash = 0
+limits_data = api.get_limits()
 
-print("Balance Cash =", balanceCash)
+limits_data = api.get_limits()
+
+if limits_data is not None:
+    available_cash = limits_data.get("data", {}).get("availablecash")
+    if available_cash is not None:
+        balance_cash = float(available_cash)
+        print("Available Cash =", balance_cash)
+    else:
+        print("Error: 'availablecash' not found in limits data.")
+else:
+    print("Error: Unable to fetch limits data.")
+
 
 # Calculate profit
-balanceCash = float(balanceCash)
-initialCash = float(initialCash)
-profit = balanceCash - initialCash
-print("Total profit =", profit)
+#balance_Cash = float(available_cash)
+#initialCash = float(initialCash)
+#profit = balance_Cash - initialCash
+#print("Total profit =", profit)
 
-time.sleep(1)
+#time.sleep(1)
 # Logout
-try:
-    time.sleep(0.4)
-    print("Logout Successful")
-except Exception as e:
-    print("Logout failed: {}".format(e.message))
+#try:
+#    time.sleep(0.4)
+#    print("Logout Successful")
+#except Exception as e:
+#    print("Logout failed: {}".format(e.message))
 
 
